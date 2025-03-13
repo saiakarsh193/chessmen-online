b6d0763950dfa91ded282a66ff9001899f79ebc3
saiakarsh193 <saiakarsh193@gmail.com>
2025-03-13 22:20:45 +0530

    added in gui: local mode, drag and drop

--- gui.py
+++ gui.py
@@ -4,9 +4,9 @@ from typing import List, Optional
 
 from pyglet import image
 from avour import Avour, COORD2FLOAT
+from avour.utils.vector import Vector2D
 from avour.utils.math import mapper_1d as mapper
-from chessmen import chessmenClient, chessmenMove, chessmenBoardUtility as CBU, COORD, FEN, PIECE_COLOR
-from chessmen.engine import START_FEN
+from chessmen import chessmenClient, chessmenMove, chessmenBoardUtility as CBU, COORD, FEN, PIECE_COLOR, START_FEN
 
 class chessmenBoard:
     def __init__(self, user_id: str, sheet_path: str = 'imgs/piece_sheet.png') -> None:
@@ -34,6 +34,8 @@ class chessmenBoard:
         self.wait_dot_count = 0
         self.wait_dot_last_update = time.time()
         self.wait_dot_update_interval = 0.5 # seconds
+        self.mouse_drag_current: COORD2FLOAT = None
+        self.mouse_drag_min_dist = 10
 
         # mouse drag variables
         self.selected_coord: COORD = None
@@ -41,23 +43,23 @@ class chessmenBoard:
     
     def load_image_data(self, path: str) -> None:
         sheet = image.load(path)
-        piece_width, piece_height = 130, 130
+        self.piece_width, self.piece_height = 130, 130
         self.image_data = {
             'black': {
-                'p': sheet.get_region(0, 307, piece_width, piece_height), # pawn
-                'r': sheet.get_region(0, 460, piece_width, piece_height), # rook
-                'n': sheet.get_region(149, 462, piece_width, piece_height), # knight
-                'b': sheet.get_region(297, 465, piece_width, piece_height), # bishop
-                'q': sheet.get_region(445, 465, piece_width, piece_height), # queen
-                'k': sheet.get_region(596, 465, piece_width, piece_height), # king
+                'p': sheet.get_region(0, 307, self.piece_width, self.piece_height), # pawn
+                'r': sheet.get_region(0, 460, self.piece_width, self.piece_height), # rook
+                'n': sheet.get_region(149, 462, self.piece_width, self.piece_height), # knight
+                'b': sheet.get_region(297, 465, self.piece_width, self.piece_height), # bishop
+                'q': sheet.get_region(445, 465, self.piece_width, self.piece_height), # queen
+                'k': sheet.get_region(596, 465, self.piece_width, self.piece_height), # king
             },
             'white': {
-                'p': sheet.get_region(0, 155, piece_width, piece_height), # pawn
-                'r': sheet.get_region(0, 10, piece_width, piece_height), # rook
-                'n': sheet.get_region(149, 12, piece_width, piece_height), # knight
-                'b': sheet.get_region(297, 15, piece_width, piece_height), # bishop
-                'q': sheet.get_region(445, 15, piece_width, piece_height), # queen
-                'k': sheet.get_region(596, 15, piece_width, piece_height), # king
+                'p': sheet.get_region(0, 155, self.piece_width, self.piece_height), # pawn
+                'r': sheet.get_region(0, 10, self.piece_width, self.piece_height), # rook
+                'n': sheet.get_region(149, 12, self.piece_width, self.piece_height), # knight
+                'b': sheet.get_region(297, 15, self.piece_width, self.piece_height), # bishop
+                'q': sheet.get_region(445, 15, self.piece_width, self.piece_height), # queen
+                'k': sheet.get_region(596, 15, self.piece_width, self.piece_height), # king
             }
         }
     
@@ -92,6 +94,26 @@ class chessmenBoard:
 
         # else status is 'in_match': found a match
         board = self.board_state.board
+
+        # drag piece
+        ignore_selected_coord = False
+        if self.selected_coord != None and self.mouse_drag_current != None:
+            start_pos = self.coord2pos(self.selected_coord)
+            dist = (Vector2D.from_tuple(start_pos) - Vector2D.from_tuple(self.mouse_drag_current)).mag()
+            if dist > self.mouse_drag_min_dist:
+                ignore_selected_coord = True # dont redraw tile in grid
+                tile = board[self.selected_coord[0]][self.selected_coord[1]]
+                if tile != ' ':
+                    if tile.isupper():
+                        piece_image = self.image_data['white'][tile.lower()]
+                    else:
+                        piece_image = self.image_data['black'][tile]
+                    piece_pos = (
+                        self.mouse_drag_current[0] - self.piece_width / 4,
+                        self.mouse_drag_current[1] + self.piece_height / 4
+                    )
+                    avour.sprite(piece_image, piece_pos, scale=0.55, level=2)
+
         # board
         avour.fill(True)
         for row in range(8):
@@ -114,21 +136,23 @@ class chessmenBoard:
                     avour.rect(pos, self.tile_size, self.tile_size, radius=(10, 0, 0, 0), segments=5)
                 else:
                     avour.rect(pos, self.tile_size, self.tile_size)
-                if self.black_side_view:
-                    tile = board[7 - row][7 - col]
+                if self.black_side_view: # convert draw coord to abstract coord
+                    drow, dcol = 7 - row, 7 - col
                 else:
-                    tile = board[row][col]
+                    drow, dcol = row, col
+                tile = board[drow][dcol]
                 # chess piece
                 if tile != ' ':
-                    if tile.isupper():
-                        piece_image = self.image_data['white'][tile.lower()]
-                    else:
-                        piece_image = self.image_data['black'][tile]
-                    piece_pos = (
-                        pos[0] + 5,
-                        pos[1] - 5
-                    )
-                    avour.sprite(piece_image, piece_pos, scale=0.55)
+                    if not (ignore_selected_coord and self.selected_coord == (drow, dcol)): # if in drag mode, ignore piece
+                        if tile.isupper():
+                            piece_image = self.image_data['white'][tile.lower()]
+                        else:
+                            piece_image = self.image_data['black'][tile]
+                        piece_pos = (
+                            pos[0] + 5,
+                            pos[1] - 5
+                        )
+                        avour.sprite(piece_image, piece_pos, scale=0.55)
                 # row marking
                 if col == 0:
                     text_color = self.C_WHITE if (row + col) % 2 else self.C_GREEN
@@ -202,13 +226,28 @@ class chessmenBoard:
     
     def _set_piece_selection(self, coord: COORD) -> None:
         self.selected_coord = coord
-        self.valid_moves = CBU.get_valid_moves(coord, self.board_state)        
+        self.valid_moves = CBU.get_valid_moves(coord, self.board_state)
 
     def _reset_piece_selection(self) -> None:
         self.selected_coord = None
         self.valid_moves = []
     
+    def on_mousedrag(self, pos: COORD2FLOAT) -> None:
+        self.mouse_drag_current = pos
+    
+    def on_mouseup(self, pos: COORD2FLOAT) -> None:
+        if self.selected_coord != None and self.mouse_drag_current != None:
+            start_pos = self.coord2pos(self.selected_coord)
+            dist = (Vector2D.from_tuple(start_pos) - Vector2D.from_tuple(pos)).mag()
+            if dist > self.mouse_drag_min_dist:
+                self.handle_click(pos)
+        self.mouse_drag_current = None
+
     def on_mousedown(self, pos: COORD2FLOAT) -> None:
+        self.handle_click(pos)
+        self.mouse_drag_current = None
+
+    def handle_click(self, pos: COORD2FLOAT) -> None:
         if not self.user_turn:
             return
         if pos[0] >= self.board_pos[0] and pos[0] <= self.board_pos[0] + self.board_size and pos[1] <= self.board_pos[1] and pos[1] >= self.board_pos[1] - self.board_size:
@@ -237,9 +276,40 @@ class chessmenBoard:
                     # if piece has already been selected, move it to opposite piece tile
                     if self.selected_coord != None:
                         self._move_piece_and_update_server_response(coord)
-    
-    def on_mousedrag(self, pos: COORD2FLOAT) -> None:
-        ...
+
+class chessmenGUILocal(Avour):
+    def __init__(self) -> None:
+        super().__init__(screen_title='chessmen local', show_fps=True)
+        self.set_frame_rate(80)
+        screen_size = self.get_screen_size()
+        self.translate((screen_size[0] / 2, screen_size[1] / 2))
+        
+        self.board = chessmenBoard(user_id='')
+        self.board.user_turn = True
+
+    def on_keydown(self, key: str) -> None:
+        if key == 'Q' or key == 'ESCAPE':
+            self.exit()
+
+    def on_mousedrag(self, pos: COORD2FLOAT, button: str) -> None:
+        self.board.on_mousedrag(pos)
+
+    def on_mouseup(self, pos: COORD2FLOAT, button: str) -> None:
+        self.board.on_mouseup(pos)
+
+    def on_mousedown(self, pos: COORD2FLOAT, button: str) -> None:
+        self.board.on_mousedown(pos)
+
+    def draw(self) -> None:
+        self.board.draw(self, status='in_match')
+
+    def loop(self, dt: float) -> None:
+        # if there is a waiting response from user
+        if self.board.response_ready:
+            self.board.user_color = self.board.board_state.active_color
+            self.board.black_side_view = self.board.user_color == 'black'
+            self.board.user_turn = True
+            self.board.response_ready = False
 
 class chessmenGUI(Avour):
     REFRESH_TIME = 1 # seconds
@@ -263,6 +333,9 @@ class chessmenGUI(Avour):
 
     def on_mousedrag(self, pos: COORD2FLOAT, button: str) -> None:
         self.board.on_mousedrag(pos)
+
+    def on_mouseup(self, pos: COORD2FLOAT, button: str) -> None:
+        self.board.on_mouseup(pos)
 
     def on_mousedown(self, pos: COORD2FLOAT, button: str) -> None:
         self.board.on_mousedown(pos)
@@ -299,7 +372,11 @@ class chessmenGUI(Avour):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='chessmen client GUI interface')
     parser.add_argument('user_id', help='username for client', type=str)
+    parser.add_argument('--local', help='play local match', action='store_true')
     args = parser.parse_args()
 
-    app = chessmenGUI(args.user_id)
+    if args.local:
+        app = chessmenGUILocal()
+    else:
+        app = chessmenGUI(args.user_id)
     app.run()
